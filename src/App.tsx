4494184dd4bb8b5dfcf7378bf1f95fa9aebc9540
jaffamonkey <paullittlebury@gmail.com>
2025-03-13 17:49:53 +0000

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -208,7 +208,7 @@ const demo4Images: string[] = [];
 /* Demo 4 images generated in Midjourney by Manoela Ilic:
 https://github.com/codrops/GridItemHoverEffect/
 */
-for (let i = 1; i <= 33; i++) {
+for (let i = 2; i <= 33; i++) {
   demo4Images.push(`./demo4/${i}.jpg`);
 }
 
