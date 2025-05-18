ddec2a2c7927fc4b530c703d1f9c2fad8c2e54ab
jaffamonkey <paullittlebury@gmail.com>
2025-05-18 15:22:04 +0100

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -62,11 +62,13 @@ function App() {
           <img className="github" src="./images/favicon.png" alt="Terrormar" />
         </a>
         <h1>Terrormar</h1>
+        <div className="spiel">
         <small>
           <p className="line-1 anim-typewriter">
           Come to Terrormar, the idyllic island destination where fear is a distant memory
           </p>
         </small>
+        </div>
       </div>
       {demo === 1 ? (
         <Demo1 />
