74b8c167628d0f7060f08e1dac92edcd91539bfb
jaffamonkey <paullittlebury@gmail.com>
2025-07-14 23:12:37 +0100

    updates

--- src/lib/Divz/components/Menubar.tsx
+++ src/lib/Divz/components/Menubar.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 
+const [setDemo] = useState<number>(2);
+
 export const Menubar: React.FC = () => {
   const [visible, setVisible] = useState<boolean>(false);
 
@@ -15,7 +17,11 @@ export const Menubar: React.FC = () => {
 
       {visible && (
         <div className="menubar">
-          <a key="3" className="fa fa-camera" onClick="setDemo(3);return false;" />
+          <a
+            key="3"
+            className="fa fa-camera"
+            onClick={() => setDemo(3)}
+          />
           <a className="fa fa-instagram" href="https://www.instagram.com/thelillyguildtrust" />
           <a className="fa fa-envelope" href="mailto:terrormarclub@gmail.com" />
           <a className="fa fa-phone" href="tel:+447394074047" />
